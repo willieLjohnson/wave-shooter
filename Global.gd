e03e4536430d00597af32353e20c4be6f697ffad
willieLjohnson <liwa.johnson@gmail.com>
2020-06-09 16:23:25 -0400

    Create global script

---
+++ Global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func instance_node(node, location, parent):
+	var node_instance = node.instance()
+	parent.add_child(node_instance)
+	node_instance.global_position = location
+	return node_instance  
