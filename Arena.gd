f0f6d10c128d5d88bf314523d26a3680872367b4
willieLjohnson <liwa.johnson@gmail.com>
2020-06-10 11:26:12 -0400

    Add enemy spawner

--- Arena.gd
+++ Arena.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+var SEEKER_ENEMY = preload("res://Enemy.tscn")
+
 func _ready() -> void:
 	Global.node_creation_parent = self
 	
 func _exit_tree() -> void:
 	Global.node_creation_parent = null
+
+
+func _on_EnemySpawnTimer_timeout() -> void:
+	var enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	
+	while enemy_position.x > 640 and enemy_position.x > -80 or enemy_position.y < 360 and enemy_position.y > -45:
+		enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	
+	Global.instance_node(SEEKER_ENEMY, enemy_position, self)
