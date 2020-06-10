732645a3eac3982b4bb2cda97cccba88e17cb3b3
willieLjohnson <liwa.johnson@gmail.com>
2020-06-10 10:16:22 -0400

    Add reload speed for shooting continously

--- Player.gd
+++ Player.gd
@@ -5,6 +5,7 @@ var velocity = Vector2.ZERO
 
 var bullet = preload("res://Bullet.tscn")
 
+var can_shoot = true
 func _process(delta: float) -> void:
 	velocity.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	velocity.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
@@ -13,5 +14,10 @@ func _process(delta: float) -> void:
 	
 	global_position += speed * velocity * delta
 	
-	if Input.is_action_just_pressed("shoot") and Global.node_creation_parent != null:
+	if Input.is_action_pressed("shoot") and Global.node_creation_parent != null and can_shoot:
 		Global.instance_node(bullet, global_position, Global.node_creation_parent)
+		$ReloadSpeed.start()
+		can_shoot = false
+
+func _on_Timer_timeout() -> void:
+	can_shoot = true
