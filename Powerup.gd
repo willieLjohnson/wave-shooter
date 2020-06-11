4e163e7cfe34faa7fcff10283b446028169f3675
willieLjohnson <liwa.johnson@gmail.com>
2020-06-11 11:00:11 -0400

    Refactor powerup system

---
+++ Powerup.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+export(String) var player_variable_modify
+export(float) var player_variable_set
+export(float) var powerup_duration = 2
+
+func _on_HitBox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		area.get_parent().set(player_variable_modify, player_variable_set)
+		area.get_parent().get_node("PowerupDuration").wait_time = powerup_duration
+		area.get_parent().get_node("PowerupDuration").start()
+		area.get_parent().powerup_reset.append(name)
+		queue_free()
