b8a879056fa17251fa62331b9b38ef345896f2c2
willieLjohnson <liwa.johnson@gmail.com>
2020-06-11 08:19:52 -0400

    add screen shake timer

---
+++ Camera2D.gd
@@ -0,0 +1,26 @@
+extends Camera2D
+
+var screen_shake_started = false
+var shake_intensity = 0
+
+func _ready() -> void:
+	Global.camera = self
+	
+func _exit_tree() -> void:
+	Global.camera = null
+	
+func _process(delta: float) -> void:
+	zoom = lerp(zoom, Vector2(1, 1), 0.3)
+	
+	if screen_shake_started:
+		global_position += Vector2(rand_range(-shake_intensity, shake_intensity), rand_range(-shake_intensity, shake_intensity))
+
+func screen_shake(intensity, time):
+	zoom = Vector2(1, 1) - Vector2(intensity * 0.02, intensity * 0.02)
+	shake_intensity = intensity
+	$ScreenShakeTime.wait_time = time
+	$ScreenShakeTime.start()
+	screen_shake_started = true
+
+func _on_ScreenShakeTime_timeout() -> void:
+	screen_shake_started = false
---
+++ Score.gd
@@ -0,0 +1,5 @@
+extends Label
+
+
+func _process(delta: float) -> void:
+	text = str(Global.score)
