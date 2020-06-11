1bed751432bba8e9ad873b4ed9baba8b236a8ac2
willieLjohnson <liwa.johnson@gmail.com>
2020-06-11 10:14:30 -0400

    Improve shooting mechanic

--- Bullet.gd
+++ Bullet.gd
@@ -1,16 +1,25 @@
 extends Sprite
 
 var velocity = Vector2(1, 0)
-var speed = 250
+var base_speed = 250
+var speed = base_speed
+var initial_speed = base_speed * 3.5
 
 var look_once = true
 
+var current_scale = scale
+
+func _ready() -> void:
+	set_scale(Vector2(0.5 + velocity.x, 0.5 + velocity.y))
+	speed = initial_speed
+	
 func _process(delta: float) -> void:
 	if look_once:
 		look_at(get_global_mouse_position())
 		look_once = false
 	global_position += velocity.rotated(rotation) * speed * delta
-
+	scale = lerp(scale, current_scale, 0.15)
+	speed = lerp(speed, base_speed, 0.15)
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
